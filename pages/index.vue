888d709f62ba9e8586f3f8e66e34fd453d98160e
luismariotti1 <luismariottip@gmail.com>
2023-09-20 19:57:12 -0300

    image

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="flex items-center justify-center">
-    <h1 class="text-xl">Hello World</h1>
+  <div class="flex flex-col items-center justify-center gap-2">
+    <h1 class="text-xl">Olá, aqui é Luis</h1>
+    <img class="w-32 h-32 rounded-full" src="../public/images/IMG_7609.JPG">
   </div>
 </template>
