2ad18fda469890d4882ab71e9b0f39f71e26151c
luismariotti1 <luismariottip@gmail.com>
2023-09-22 18:13:30 -0300

    principais tecnologias

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,27 @@
       <HomeHello />
       <HomeCode />
       <HomeBio />
+      <div class="flex flex-col gap-4">
+        <h2 class="text-2xl text-primary-300">Principais tecnologias</h2>
+        <div class="rounded-md bg-surface-100 bg-opacity-60">
+          <div>
+            <div class="flex flex-col items-center justify-center p-4">
+              <strong class="capitalize text-accent-200">Front-end</strong>
+              <ul>
+                <li class="flex items-center gap-2">
+                  <nuxt-icon name="html5" /> HTML
+                </li>
+                <li class="flex items-center gap-2">
+                  <nuxt-icon name="css3" /> CSS
+                </li>
+                <li class="flex items-center gap-2">
+                  <nuxt-icon name="vuejs" /> Vue.js
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
