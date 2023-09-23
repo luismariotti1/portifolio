a49a80e4517c4885db87e22b3cb8e959b4c0cd9f
luismariotti1 <luismariottip@gmail.com>
2023-09-23 17:53:38 -0300

    starting mobile nav bar

---
+++ components/Home/Projects.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="flex w-full flex-col gap-4">
+    <h2 class="text-center text-2xl text-primary-300">Projetos</h2>
+    <div class="flex w-full flex-col gap-2 p-2">
+      <div class="rounded-md bg-surface-100 bg-opacity-60 p-4">
+        <div class="flex w-full flex-col items-center justify-center gap-2">
+          <h3 class="text-xl uppercase text-accent-100">
+            <strong>Antares Solar</strong>
+          </h3>
+          <img src="/img/AntaresSolar.png" alt="projeto antares" />
+          <p class="text-justify text-accent-200">
+            Antares Solar é um <strong>SAAS</strong>, desenvolvido para
+            instaladoras de energia solar. O software oferece como principal
+            recurso a <strong>geração de propostas</strong> de sistemas
+            fotovoltaicos extremamente elegantes e com recursos inéditos como
+            consulta via API de <strong>irradiância solar</strong> e
+            <strong>cálculo da aréa</strong>
+            disponível para instalação por satélites.
+          </p>
+          <div class="flex w-full flex-col gap-2">
+            <div
+              class="flex w-full items-center justify-center gap-2 rounded-md bg-primary-300 p-2"
+            >
+              <span>Mais informações</span>
+            </div>
+            <a href="https://antaressolar.com" target="_blank">
+              <div
+                class="flex items-center justify-center gap-2 rounded-md bg-primary-300 p-2 text-center"
+              >
+                Visitar site
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="rounded-md bg-surface-100 bg-opacity-60 p-4">
+            <div class="flex w-full flex-col items-center justify-center gap-2">
+              <h3 class="text-xl uppercase text-accent-100">
+                <strong>Truco Max</strong>
+              </h3>
+              <img src="/img/AntaresSolar.png" alt="projeto antares" />
+              <p class="text-justify text-accent-200">
+                Truco Max é um jogo Free to Play de truco online
+              </p>
+              <div class="flex w-full flex-col gap-2">
+                <div
+                  class="flex w-full items-center justify-center gap-2 rounded-md bg-primary-300 p-2"
+                >
+                  <span>Mais informações</span>
+                </div>
+                <a href="https://antaressolar.com" target="_blank">
+                  <div
+                    class="flex items-center justify-center gap-2 rounded-md bg-primary-300 p-2 text-center"
+                  >
+                    Visitar site
+                  </div>
+                </a>
+              </div>
+            </div>
+          </div> -->
+    </div>
+  </div>
+</template>
--- components/Page/MobileNavBar.vue
+++ components/Page/MobileNavBar.vue
@@ -1,7 +1,39 @@
 <template>
-  <div class="fixed right-3 top-3 z-50">
-    <div class="rounded-md bg-primary-300">
-      <Icon name="ic:outline-menu" color="black" class="h-8 w-8" />
+  <div>
+    <div @click="toggleNav" class="fixed right-3 top-3 z-50">
+      <div class="rounded-md bg-primary-300">
+        <Icon
+          :name="!showNav ? 'ic:outline-menu' : 'ic:outline-close'"
+          color="black"
+          class="h-8 w-8"
+        />
+      </div>
+    </div>
+    <div v-if="showNav">
+      <div class="fixed z-40 h-full w-full bg-surface-100 bg-opacity-90">
+        <div class="flex h-full flex-col gap-4 px-6 py-12">
+          <div class="text-accent-200">Sobre</div>
+          <div class="text-accent-200">Tecnologias</div>
+          <div class="text-accent-200">Projetos</div>
+          <div class="text-accent-200">Contato</div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+export default {
+  name: "MobileNavBar",
+  data() {
+    return {
+      showNav: false,
+    };
+  },
+  methods: {
+    toggleNav() {
+      this.showNav = !this.showNav;
+    },
+  },
+};
+</script>
