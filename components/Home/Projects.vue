1ad1e095c7ec52cf8f80d1ec88bd22d4ec9b4146
luismariotti1 <luismariottip@gmail.com>
2023-09-24 14:44:08 -0300

    animation projects

--- components/Home/Projects.vue
+++ components/Home/Projects.vue
@@ -2,7 +2,11 @@
   <div class="flex w-full flex-col gap-4">
     <h2 class="text-center text-2xl text-primary-300">Projetos</h2>
     <div class="flex w-full flex-col gap-2 p-2">
-      <div class="rounded-md bg-surface-100 bg-opacity-60 p-4">
+      <div
+        data-aos="flip-right"
+        data-aos-duration="1200"
+        class="rounded-md bg-surface-100 bg-opacity-60 p-4"
+      >
         <div class="flex w-full flex-col items-center justify-center gap-2">
           <h3 class="text-xl uppercase text-accent-100">
             <strong>Antares Solar</strong>
@@ -10,12 +14,14 @@
           <img src="/img/AntaresSolar.png" alt="projeto antares" />
           <p class="text-justify text-accent-200">
             Antares Solar é um <strong>SAAS</strong>, desenvolvido para
-            instaladoras de energia solar. O software oferece como principal
-            recurso a <strong>geração de propostas</strong> de sistemas
-            fotovoltaicos extremamente elegantes e com recursos inéditos como
-            consulta via API de <strong>irradiância solar</strong> e
-            <strong>cálculo da aréa</strong>
-            disponível para instalação por satélites.
+            instaladoras de energia solar. O software oferece ao seus usuário
+            <strong>geração de propostas</strong> de sistemas fotovoltaicos
+            <strong>extremamente elegantes e precisas</strong>, além de trazer
+            um recurso inédito para está modadalidade de software, permitindo
+            <strong
+              >calcular a área de instalação por meio de imagens de
+              satélite</strong
+            >.
           </p>
           <div class="flex w-full flex-col gap-2">
             <div
