7698a7e7f8d69518ab5a580f587d812f56db587e
luismariotti1 <luismariottip@gmail.com>
2023-11-20 12:06:10 -0300

    add TrucoMax to projects

--- components/Home/Projects.vue
+++ components/Home/Projects.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="xlg:w-2/3 flex w-full flex-col gap-4">
+  <div class="flex w-full flex-col gap-4 xlg:w-3/5">
     <h2 class="text-center text-2xl text-primary-300">Projetos</h2>
     <div class="flex w-full flex-col gap-6">
       <div
@@ -8,22 +8,19 @@
         class="rounded-md bg-surface-100 bg-opacity-40 p-4"
       >
         <div
-          class="xs:p-4 flex w-full flex-col items-center justify-center gap-6"
+          class="flex w-full flex-col items-center justify-center gap-6 xs:p-4"
         >
           <h3 class="text-xl uppercase text-accent-100">
             <strong>Antares Solar</strong>
           </h3>
           <img src="/img/AntaresSolar.png" alt="projeto antares" />
           <p class="text-justify text-accent-200">
-            Antares Solar é um <strong>SAAS</strong>, desenvolvido para
-            instaladoras de energia solar. O software oferece ao seus usuário
-            <strong>geração de propostas</strong> de sistemas fotovoltaicos
-            <strong>extremamente elegantes e precisas</strong>, além de trazer
-            um recurso inédito para está modadalidade de software, permitindo
-            <strong
-              >calcular a área de instalação por meio de imagens de
-              satélite</strong
-            >.
+            Antares Solar é um SAAS, desenvolvido para instaladoras de energia
+            solar. O software oferece ao seus usuário geração de propostas de
+            sistemas fotovoltaicos extremamente elegantes e precisas, além de
+            trazer um recurso inédito para está modadalidade de software,
+            permitindo calcular a área de instalação por meio de imagens de
+            satélite.
           </p>
           <div class="flex w-full flex-col gap-2">
             <a href="https://antaressolar.com" target="_blank">
@@ -42,18 +39,48 @@
         class="rounded-md bg-surface-100 bg-opacity-40 p-4"
       >
         <div
-          class="xs:p-4 flex w-full flex-col items-center justify-center gap-6"
+          class="flex w-full flex-col items-center justify-center gap-6 xs:p-4"
+        >
+          <h3 class="text-xl uppercase text-accent-100">
+            <strong>TrucoMax</strong>
+          </h3>
+          <img src="/img/TrucoMax.png" alt="projeto antares" />
+          <p class="text-justify text-accent-200">
+            TrucoMax é uma plataforma inovadora de jogo de cartas, desenvolvida
+            com a tecnologia WebSocket para assegurar uma experiência de jogo
+            contínua e em tempo real. Este jogo online gratuito revoluciona o
+            Truco tradicional ao trazê-lo para a era digital, permitindo que
+            jogadores de todo o mundo se conectem e compitam. Destaca-se por sua
+            interface intuitiva, focada em uma jogabilidade amigável e um design
+            visual impressionante.
+          </p>
+          <div class="flex w-full flex-col gap-2">
+            <a href="http://trucomax.com" target="_blank">
+              <div
+                class="flex items-center justify-center gap-2 rounded-md bg-primary-300 p-2 text-center"
+              >
+                Visitar site
+              </div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div
+        data-aos="flip-right"
+        data-aos-duration="1200"
+        class="rounded-md bg-surface-100 bg-opacity-40 p-4"
+      >
+        <div
+          class="flex w-full flex-col items-center justify-center gap-6 xs:p-4"
         >
           <h3 class="text-xl uppercase text-accent-100">
             <strong>Geo2D</strong>
           </h3>
           <img src="/img/Geo2D.png" alt="projeto antares" />
           <p class="text-justify text-accent-200">
-            Geo2D é um software de <strong>código aberto</strong> que permite
-            que os usuários <strong>criem figuras geométricas</strong> em um
-            plano cartesiano e
-            <strong>apliquem multiplas transformações</strong> geométricas sobre
-            elas.
+            Geo2D é um software de código aberto que permite que os usuários
+            criem figuras geométricas em um plano cartesiano e apliquem
+            multiplas transformações geométricas sobre elas.
           </p>
           <div class="flex w-full flex-col gap-2">
             <a href="https://github.com/luismariotti1/Geo2D" target="_blank">
